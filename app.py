a64da815723a69c8d5d8fb926c1e7f7b13bff2d8
Danielle Hansis <hansis@mail.tau.ac.il>
2025-08-15 12:30:16 +0300

    changed app, so scss files would be auto-complied to css files, and not through flask-scss.

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask import render_template
+import sass # libsass: compiles SCSS → CSS without needing Flask-Scss
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)  # tells flask where to find files to run app
+# reads all .scss files in static/ and Writes compiled .css into the same static/ folder
+sass.compile(dirname=('static', 'static'), output_style='expanded')
 
 
 @app.route('/')  # the main route
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
